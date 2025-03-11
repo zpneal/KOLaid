01ffdc4b4fb29184512865ac2378464de618c581
zpneal <zpneal@msu.edu>
2025-03-11 10:01:06 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 .onAttach <- function(lib,pkg) {
   local_version <- utils::packageVersion("KOLaid")
   packageStartupMessage("KOLaid v",local_version)
-  packageStartupMessage("Cite: Neal, Z. P. and Dockerty, M. (2025). KOLaid: An R package to identify")
-  packageStartupMessage("      key opinion leaders from a network given constraints GitHub.")
-  packageStartupMessage("      https://github.com/zpneal/KOLaid/")
+  packageStartupMessage("Cite: Neal, Z. P. and Dockerty, M. (2025). KOLaid: An R package to identify key opinion")
+  packageStartupMessage("      leaders from a network given constraints GitHub. https://github.com/zpneal/KOLaid/")
+  packageStartupMessage("      ")
   packageStartupMessage("Help: type vignette(\"KOLaid\"); email zpneal@msu.edu; github zpneal/KOLaid")
   packageStartupMessage("Beta: type devtools::install_github(\"zpneal/KOLaid\", ref = \"devel\")")
 }
