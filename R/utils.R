2802964a8fba47540931f800e272c468e45d2013
zpneal <zpneal@msu.edu>
2025-03-12 13:54:03 -0400

    Add files via upload

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
 .onAttach <- function(lib,pkg) {
   local_version <- utils::packageVersion("KOLaid")
   packageStartupMessage("KOLaid v",local_version)
-  packageStartupMessage("Cite: Neal, Z. P. and Dockerty, M. (2025). KOLaid: An R package to identify key opinion")
-  packageStartupMessage("      leaders from a network given constraints GitHub. https://github.com/zpneal/KOLaid/")
+  packageStartupMessage("Cite: Neal, Z. P., Neal, J. W., Cappella, E., and Dockerty, M. (2025). KOLaid: A tool for selecting")
+  packageStartupMessage("      key opinion leaders under practical constraints. GitHub. https://github.com/zpneal/KOLaid/")
   packageStartupMessage("      ")
   packageStartupMessage("Help: type vignette(\"KOLaid\"); email zpneal@msu.edu; github zpneal/KOLaid")
   packageStartupMessage("Beta: type devtools::install_github(\"zpneal/KOLaid\", ref = \"devel\")")
